8a30217d8dc6d0239ec0c7c3df3b1bad7d995763
dev-rfgul <rfgul587@gmail.com>
2024-11-29 01:32:35 +0500

    added tailwind css in the project

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header bg-green-500">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link bg-green-600"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
